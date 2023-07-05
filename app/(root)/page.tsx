6105e38892bf632923c59c0662f2b58be3e4d4fc
NatiFekadu <natifekadu808@gmail.com>
2023-07-05 17:22:09 +0300

    Clerk Authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+
+const SetupPage =() => {
     return (
     <div className="p-4">
-      <p>Hello Admin Dashboard</p>
-      <Button>Equb</Button>
+      <UserButton afterSignOutUrl="/"/>
+  
     </div>
     )
   }
   
+  export default SetupPage
