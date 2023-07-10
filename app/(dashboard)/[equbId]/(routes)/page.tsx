843b1f26eafc3dcf8df8814be5292f4a79ee5ddd
NatiFekadu <natifekadu808@gmail.com>
2023-07-10 12:12:34 +0300

    cards

--- app/(dashboard)/[equbId]/(routes)/page.tsx
+++ app/(dashboard)/[equbId]/(routes)/page.tsx
@@ -1,4 +1,13 @@
 import prismadb from '@/lib/prismadb';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
 interface DashboardPageProps {
   params: { equbId: string };
 }
@@ -9,7 +18,38 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
       id: params.equbId,
     },
   });
-  return <div>Active Equb: {equb?.name}</div>;
+  return (
+    <div>
+      Active Equb: {equb?.name}
+      <div className='container ml-auto mr-auto flex flex-wrap items-start'>
+        {/* <Card>
+          <CardHeader>
+            <CardTitle>{equb?.name}</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        
+        <Card>
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card> */}
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
